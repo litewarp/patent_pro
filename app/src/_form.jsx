b9f168b5bb6fd96f9b4a1f7b29b0f25138e96c91
Nick Sethi <nicholas.sethi@gmail.com>
2019-07-06 16:49:59 -0700

    formikkk

---
+++ app/src/_form.jsx
@@ -0,0 +1,91 @@
+/** @format */
+// @flow
+
+import React from "react"
+import { Formik, ErrorMessage } from "formik"
+import { Box, Form, FormField, TextInput, Button } from "grommet"
+import * as Yup from "yup"
+import { Search, Send } from "grommet-icons"
+
+type FormikBag = {
+  values: {},
+  touched: {},
+  dirty: {},
+  errors: {},
+  handleChange: () => void,
+  handleBlur: () => void,
+  handleSubmit: () => void,
+  handleReset: () => void,
+  setFieldValue: (string, mixed) => void,
+  setFieldTouched: (string, mixed) => void,
+  isSubmitting: boolean,
+}
+
+const numberSchema = Yup.object().shape({
+  patentNumber: Yup.number()
+    .lessThan(8, "Number must be 7 digits (for now)")
+    .moreThan(6, "Number must be 7 digits (for now)")
+    .required("A Valid Patent Number is Required"),
+})
+
+const PatentNumberForm = ({
+  // redux
+  options,
+  createPatent,
+  fetchPatentNumbers,
+}: {
+  options: Array<{}>,
+  createPatent: number => void,
+  fetchPatentNumbers: string => void,
+}) => (
+  <Formik
+    validateOnChange={false}
+    initialValues={{ patentNumber: "" }}
+    onSubmit={(values, { setSubmitting }) => {
+      createPatent(values.patentNumber)
+      setSubmitting(false)
+    }}
+    validationSchema={numberSchema}
+    render={({
+      values,
+      touched,
+      dirty,
+      errors,
+      handleChange,
+      handleBlur,
+      handleSubmit,
+      handleReset,
+      setFieldValue,
+      setFieldTouched,
+      isSubmitting,
+    }: FormikBag) => {
+      return (
+        <Form>
+          <Box align="center" fill={{ horizontal: true }} gap="large">
+            <Box direction="row" align="center" gap="medium">
+              <TextInput
+                type="search"
+                placeholder="e.g., 7629705"
+                onChange={ev => {
+                  fetchPatentNumbers(ev.target.value)
+                  setFieldValue("patentNumber", ev.target.value)
+                }}
+                onSelect={ev => {
+                  setFieldValue("patentNumber", ev.currentTarget.innerText)
+                }}
+                onBlur={() => setFieldTouched("patentNumber", true)}
+                onKeyPress={ev => (ev.key === "Enter" ? handleSubmit() : null)}
+                value={values.patentNumber}
+                name="patentNumber"
+                suggestions={options}
+              />
+            </Box>
+            <ErrorMessage name="patentNumber" component="div" />
+            <Button icon={<Send />} type="submit" gap="medium" label="Submit" />
+          </Box>
+        </Form>
+      )
+    }}
+  />
+)
+export default PatentNumberForm
--- app/src/home.jsx
+++ app/src/home.jsx
@@ -4,59 +4,29 @@
 import * as React from "react"
 import { connect } from "react-redux"
 import { bindActionCreators } from "redux"
-import { Box, Heading, FormField, TextInput } from "grommet"
-import { Search } from "grommet-icons"
+import { Box, Heading, Form, FormField, TextInput, Text } from "grommet"
 import { createPatent, fetchPatentNumbers } from "./_redux/patentActions"
+import PatentNumberForm from "./_form"
 
 const Home = ({
   fetchPatentNumbers,
   createPatent,
   patentNumbers,
 }: {
-  createPatent: string => void,
+  createPatent: number => void,
   fetchPatentNumbers: (?string) => void,
   patentNumbers: Array<Object>,
-}) => {
-  const [value, setValue] = React.useState("")
-  const suggestions = patentNumbers.map((pN, index) => pN.attributes.number)
-
-  const handleChange = val => {
-    setValue(val)
-    fetchPatentNumbers(val)
-  }
-
-  const handleSelect = val => {
-    setValue(val)
-  }
-
-  const handleKeyPress = val => {
-    if (val === "Enter") {
-      createPatent(value)
-    }
-  }
-
-  return (
-    <Box>
-      <Heading level={2}> PTO Patent PDF Parser Pro (Pre-Release) </Heading>
-      <Heading level={4}>Enter a US Patent Number</Heading>
-
-      <Box direction="row" align="center" gap="small">
-        <Search color="brand" />
-        <TextInput
-          name="patent"
-          type="search"
-          placeholder="e.g., 7629705"
-          value={value}
-          dropProps={{ height: "small" }}
-          onKeyPress={e => handleKeyPress(e.key)}
-          onChange={e => handleChange(e.target.value)}
-          onSelect={e => handleSelect(e.target.value)}
-          suggestions={suggestions}
-        />
-      </Box>
-    </Box>
-  )
-}
+}) => (
+  <>
+    <Heading level={2}> PTO Patent PDF Parser Pro (Pre-Release) </Heading>
+    <Heading level={4}>Enter a US Patent Number</Heading>
+    <PatentNumberForm
+      createPatent={createPatent}
+      fetchPatentNumbers={fetchPatentNumbers}
+      options={patentNumbers.map((pN, index) => pN.attributes.number)}
+    />
+  </>
+)
 
 const mapState = ({ patent }) => ({
   patentNumbers: patent.patentNumbers,
