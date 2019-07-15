9b66a5ee8de023bd93acb5746fcd70affa8c883b
Nick Sethi <nicholas.sethi@gmail.com>
2019-07-15 08:19:37 -0700

    remove addded in fallback url

--- api/app/controllers/api/v1/application_controller.rb
+++ api/app/controllers/api/v1/application_controller.rb
@@ -1,9 +1,6 @@
 module Api
   module V1
     class ApplicationController < ActionController::API
-      def fallback_index_html
-        render file: 'public/index.html'
-      end
 
       def allow_graphiti_debug_json?
         true
--- api/config/routes.rb
+++ api/config/routes.rb
@@ -7,8 +7,5 @@ Rails.application.routes.draw do
       resources :lines
     end
   end
-  get '*path', to: 'application#fallback_index_html', constraints: lambda { |request|
-                                                                     !request.xhr? && request.format.html?
-                                                                   }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
