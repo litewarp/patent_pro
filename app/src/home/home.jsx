ac4f012f7f71e66ffad407cf82fcecc223708222
Nick Sethi <nicholas.sethi@gmail.com>
2019-07-15 14:33:21 -0700

    one last test

--- app/src/home/home.jsx
+++ app/src/home/home.jsx
@@ -2,7 +2,7 @@
 
 import * as React from "react"
 import { shallowEqual, useSelector, useDispatch } from "react-redux"
-import { Box, Button, Grid, Heading } from "grommet"
+import { Box, Button, Grid, Heading, ResponsiveContext } from "grommet"
 import styled from "styled-components"
 import MessageModal from "./_messages"
 import ContactDetails from "./_contacts"
@@ -32,31 +32,35 @@ const LandingPage = () => {
   const [modal, showModal] = React.useState(false)
   //render
   return (
-    <Grid
-      fill
-      justifyContent="center"
-      rows={["small", "auto", "xxsmall"]}
-      columns={["auto", "auto"]}
-      areas={[
-        { name: "marquee", start: [0, 0], end: [1, 0] },
-        { name: "bullets", start: [0, 1], end: [0, 1] },
-        { name: "images", start: [1, 1], end: [1, 1] },
-        { name: "contacts", start: [0, 2], end: [1, 2] },
-      ]}
-    >
-      <Box gridArea="marquee" align="center" gap="small" margin="small">
-        <Title margin="none" level={1}>
-          PATENT PRO
-        </Title>
-        <Heading level={3} margin="none">
-          Automatically generate snippets from US Patents
-        </Heading>
-      </Box>
-      <BulletPoints />
-      <ImageMockups patentProps={column && column} />
-      <ContactDetails />
-      {modal && <MessageModal showModal={showModal} modal={modal} />}
-    </Grid>
+    <ResponsiveContext.Consumer>
+      {size => (
+        <Grid
+          fill
+          justifyContent="center"
+          rows={["small", "auto", "xxsmall"]}
+          columns={["auto", "auto"]}
+          areas={[
+            { name: "marquee", start: [0, 0], end: [1, 0] },
+            { name: "bullets", start: [0, 1], end: [0, 1] },
+            { name: "images", start: [1, 1], end: [1, 1] },
+            { name: "contacts", start: [0, 2], end: [1, 2] },
+          ]}
+        >
+          <Box gridArea="marquee" align="center" gap="small" margin="small">
+            <Title margin="none" level={1}>
+              PATENT PRO
+            </Title>
+            <Heading level={3} margin="none">
+              Automatically generate snippets from US Patents
+            </Heading>
+          </Box>
+          <BulletPoints />
+          {size === "large" && <ImageMockups patentProps={column && column} />}
+          <ContactDetails />
+          {modal && <MessageModal showModal={showModal} modal={modal} />}
+        </Grid>
+      )}
+    </ResponsiveContext.Consumer>
   )
 }
 
