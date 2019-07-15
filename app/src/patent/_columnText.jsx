de0c27c5a47f83c5e7ce9fc34d3fd172afc94e77
Nick Sethi <nicholas.sethi@gmail.com>
2019-07-15 11:58:46 -0700

    one more

--- app/src/patent/_columnText.jsx
+++ app/src/patent/_columnText.jsx
@@ -7,16 +7,17 @@ import { Heading, Table, TableBody, TableRow, TableCell, Text } from "grommet"
 const ColumnText = ({ textAsLines }: { textAsLines: Array<string> }) => (
   <Table>
     <TableBody>
-      {textAsLines.map((line, index) => (
-        <TableRow key={index + 22 * 3}>
-          <TableCell scope="row">
-            <strong>{index + 1}</strong>
-          </TableCell>
-          <TableCell>
-            <Text size="medium">{line}</Text>
-          </TableCell>
-        </TableRow>
-      ))}
+      {!!textAsLines &&
+        textAsLines.map((line, index) => (
+          <TableRow key={index + 22 * 3}>
+            <TableCell scope="row">
+              <strong>{index + 1}</strong>
+            </TableCell>
+            <TableCell>
+              <Text size="medium">{line}</Text>
+            </TableCell>
+          </TableRow>
+        ))}
     </TableBody>
   </Table>
 )
