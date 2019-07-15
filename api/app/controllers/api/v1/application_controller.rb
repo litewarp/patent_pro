3836101b7413a7f3804d0f047feaa4cf915fb7ea
Nick Sethi <nicholas.sethi@gmail.com>
2019-07-15 01:55:24 -0700

    fallback url

--- api/app/controllers/api/v1/application_controller.rb
+++ api/app/controllers/api/v1/application_controller.rb
@@ -1,6 +1,9 @@
 module Api
   module V1
     class ApplicationController < ActionController::API
+      def fallback_index_html
+        render file: 'public/index.html'
+      end
 
       def allow_graphiti_debug_json?
         true
