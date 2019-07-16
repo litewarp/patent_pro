43762f75b67659dcac1efe0a87e132f05fc9b922
Nick Sethi <nicholas.sethi@gmail.com>
2019-07-15 21:10:25 -0700

    production builds and react-router gets fallback

--- api/app/controllers/application_controller.rb
+++ api/app/controllers/application_controller.rb
@@ -1,9 +1,6 @@
 class ApplicationController < ActionController::API
   def fallback_index_html
-    puts Rails.public_path
-    file = File.open("#{Rails.public_path}/index.html")
-    puts file.read
-    render :file => [Rails.public_path, "index.html"].join("/")
+    render :html => File.read([Rails.public_path, "index.html"].join("/")).html_safe
   end
   def allow_graphiti_debug_json?
     true
