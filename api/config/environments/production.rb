41aacc9ea623458e0360c675d4432227359c04e3
Nick Sethi <nicholas.sethi@gmail.com>
2019-07-17 19:28:56 -0700

    add host in production

--- api/config/environments/production.rb
+++ api/config/environments/production.rb
@@ -17,6 +17,8 @@ Rails.application.configure do
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
   config.require_master_key = true
+  Rails.application.routes.default_url_options[:host] = "mccoybot.com"
+  config.action_controller.default_url_options = { host: 'mccoybot.com' }
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
